51cf0239da2e593521eb6cd812f4ed10e30b9121
AtlantaPatterson <atlantarpatterson@gmail.com>
2022-08-14 14:29:05 -0700

    commit tree ae322169c3bbd1338f8ce93d177844db8c5f06dd

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex) 
+VALUES 
+("Mr. Chocolate", 20, "M"),
+("Rowdy", 10, "M"),
+("Sargeant Brown", 19, "M"),
+("Grinch", 2, "M"),
+("Melissa", 13, "F"),
+("Wendy", 6, "F"),
+("Wendy", 6, "F"),
+(NULL, 3, "F");
